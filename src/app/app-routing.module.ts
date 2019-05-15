dd9fabb08eca4a7cddd1ea80fea5ccac51062ca6
PedroSchwarz <pedroschawarz97@gmail.com>
2019-05-15 01:57:53 -0300

    Tabs added to places page, routing updated in places-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
-  { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
-  { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
-  { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
-  { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
-  { path: 'place-detail', loadChildren: './places/discover/place-detail/place-detail.module#PlaceDetailPageModule' },
-  { path: 'offer-booking', loadChildren: './places/offers/offer-booking/offer-booking.module#OfferBookingPageModule' },
   { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' },
 ];
 
