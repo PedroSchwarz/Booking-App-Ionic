9efac74d6c04904494b0af1b0ee68d2c99598519
PedroSchwarz <pedroschawarz97@gmail.com>
2019-05-17 00:19:29 -0300

    Create booking modal added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CreateBookingComponent } from './bookings/create-booking/create-booking.component';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -12,7 +13,12 @@ import { Place } from '../../place.model';
 export class PlaceDetailPage implements OnInit {
   place: Place;
 
-  constructor(private activatedRoute: ActivatedRoute, private placesService: PlacesService, private navCtrl: NavController) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private placesService: PlacesService,
+    private navCtrl: NavController,
+    private modalCtrl: ModalController
+    ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -25,4 +31,14 @@ export class PlaceDetailPage implements OnInit {
     });
   }
 
+  onBookPlace() {
+    this.modalCtrl.create({component: CreateBookingComponent, componentProps: {selectedPlace: this.place}}).then(modalEl => {
+      modalEl.present();
+      return modalEl.onDidDismiss();
+    }).then(resultData => {
+      if (resultData.role === 'confirm') {
+        console.log('Booked');
+      }
+    });
+  }
 }
