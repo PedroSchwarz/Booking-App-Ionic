c4db2ca5034b22fc44233e9b5ef2ae976cd33a7b
PedroSchwarz <pedroschawarz97@gmail.com>
2019-05-21 01:36:19 -0300

    Create booking form added

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Place } from 'src/app/places/place.model';
 import { ModalController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-booking',
@@ -9,16 +10,55 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() mode: 'select' | 'random';
+  @ViewChild('form') form: NgForm;
+
+  startDate: string;
+  endDate: string;
 
   constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availablTo = new Date(this.selectedPlace.availableTo);
+    if (this.mode === 'random') {
+      this.startDate = new Date(
+        availableFrom.getTime() +
+        Math.random() *
+        (availablTo.getTime() -
+        7 * 24 * 60 * 60 * 1000 -
+        availableFrom.getTime())).toISOString();
+
+      this.endDate = new Date (
+        new Date(this.startDate).getTime() +
+        Math.random() *
+        (new Date(this.startDate).getTime() +
+        6 * 24 * 60 * 60 * 1000 -
+        new Date(this.startDate).getTime())).toISOString();
+    }
+  }
 
   onBookPlace() {
-    this.modalCtrl.dismiss({message: 'This is a dummy message'}, 'confirm');
+    if (!this.form.valid || !this.datesValidator()) {
+      return;
+    }
+    const values = this.form.value;
+    this.modalCtrl.dismiss({bookingData: {
+      firstName: values['first-name'],
+      lastName: values['last-name'],
+      guestNumber: values['guest-number'],
+      startDate: values['date-from'],
+      endDate: values['date-to']
+    }}, 'confirm');
   }
 
   onCancel() {
     this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  datesValidator() {
+    const startDate = new Date(this.form.value['date-from']);
+    const endDate = new Date(this.form.value['date-to']);
+    return endDate > startDate;
   }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -10,19 +10,25 @@ export class PlacesService {
       'p1', 'Manhattan Mansion',
       'In the heart of New York City',
       'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
-      149.99
+      149.99,
+      new Date('2019-02-02'),
+      new Date('2020-02-02')
     ),
     new Place(
       'p2', 'Osaka Temple',
       'Mixing modern with acient culture',
       'https://cdn.sweetescape.com/images/cities/osaka/cover/ba725feb-46fa-4913-b321-68e74c75d2ce-1920.jpg',
-      260.00
+      260.00,
+      new Date('2019-02-02'),
+      new Date('2020-02-02')
     ),
     new Place(
       'p3', 'Gramado House',
       'Romantic city, clean, nice weather and people!',
       'https://cdn.zarpo.com.br/media/catalog/product/cache/1/base/640x360/9df78eab33525d08d6e5fb8d27136e95/g/r/gramado_arquitetura_2.jpg',
-      800.00
+      800.00,
+      new Date('2019-02-02'),
+      new Date('2020-02-02')
     ),
   ];
 
